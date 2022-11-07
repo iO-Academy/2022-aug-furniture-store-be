1f31357907511f7854f9cb9eb358e01f73ae0fdc
pedrofn <pedrofechado@gmail.com>
2022-11-07 17:03:26 +0000

    added CategoryService service

--- src/Services/CategoryService.php
+++ src/Services/CategoryService.php
@@ -1,6 +1,30 @@
 <?php
+namespace Fsbe\Services;
+
+use Fsbe\DataAccess\Database;
+use Fsbe\DataAccess\CategoryDAO;
+use Fsbe\Entities\Category;
+use Fsbe\Entities\Categories;
 
 class CategoryService
 {
+    private Database $db;
 
+    /**
+     * @param Database $db
+     */
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    public function getCategory(int $id): Category
+    {
+        return CategoryDAO::fetch($this->db, $id);
+    }
+
+    public function getCategories(): Categories
+    {
+        return CategoriesDAO::fetch($this->db);
+    }
 }
