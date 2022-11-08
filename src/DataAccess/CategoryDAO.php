33819c20e4d75fc3a015d83741b18b1ea0a7c687
Fatima <fzms90@gmail.com>
2022-11-08 10:33:06 +0000

    blank line deleted

--- src/DataAccess/CategoryDAO.php
+++ src/DataAccess/CategoryDAO.php
@@ -5,7 +5,6 @@ namespace Fsbe\DataAccess;
 use Fsbe\DataAccess\Hydrators\CategoryHydrator;
 use Fsbe\Entities\Category;
 
-
 class CategoryDAO
 {
     public static function fetch(Database $db, int $id): Category
