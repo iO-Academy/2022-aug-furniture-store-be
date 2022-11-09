2f7a12781ddc025412b4971fe0f9f8115afcc2b0
Fatima <fzms90@gmail.com>
2022-11-09 11:59:37 +0000

    category validator created

--- src/Services/Validators/CategoryValidator.php
+++ src/Services/Validators/CategoryValidator.php
@@ -1,6 +1,20 @@
 <?php
 
+namespace Fsbe\Services\Validators;
+
+use Fsbe\Services\CategoryService;
+
 class CategoryValidator
 {
+    public static function validateCategory(int $catId): bool
+    {
+        $categoryService = new CategoryService();
+        $categories = $categoryService->getCategories();
+        $count = count($categories->getCategories()); // change to check for id, include try catch here
 
+        $filtered = array_filter($categories, function($category) use ($catId) {
+                return $category->getId() === $catId;
+        });
+        return (bool)count($filtered);
+    }
 }
