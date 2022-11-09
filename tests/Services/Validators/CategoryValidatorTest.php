dfa7d083f57552c04570605d475503e1710bf174
pedrofn <pedrofechado@gmail.com>
2022-11-09 17:00:46 +0000

    removed comments from ValidateCategoryTest

--- tests/Services/Validators/CategoryValidatorTest.php
+++ tests/Services/Validators/CategoryValidatorTest.php
@@ -6,23 +6,17 @@ use PHPUnit\Framework\TestCase;
 
 class CategoryValidatorTest extends TestCase
 {
-    // success tests
     public function testValidateCategory_GivenCategoryId1ReturnsTrue()
     {
-        // Arrange
-        // Act
         $actual = \Fsbe\Services\Validators\CategoryValidator::validateCategory(1);
-        // Assert
+
         $this->assertEquals(true, $actual);
     }
 
-    // Failure tests
     public function testValidateCategory_GivenCategoryId99ReturnsFalse()
     {
-        // Arrange
-        // Act
         $actual = \Fsbe\Services\Validators\CategoryValidator::validateCategory(99);
-        // Assert
+
         $this->assertEquals(false, $actual);
     }
 }
